19332ed498102500bdfd1507b8e3b701f1294fa3
JCGuest <jcguest90@gmail.com>
2020-02-25 17:02:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// function writeCards(names) {
+//     let thankYouCards = []
+//     for (let i = 0; i < names.length; i++){
+//     thankYouCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!.`)
+//     }    
+//     return thankYouCards
+// }
+
+ 
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+// 'Thank you, Lisa, for the wonderful surprise gift!'
+
+function countDown(number) {
+    while (number > 0 ) {
+        console.log(number);
+        number-=1;
+    }
+    console.log(number);
+}
+
