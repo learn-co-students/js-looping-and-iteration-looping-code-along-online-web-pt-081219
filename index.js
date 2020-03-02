f57a8999b104361d7b26fd260c560ddc8c44422d
mikeisfake <mikescooper@icloud.com>
2020-03-02 01:07:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return cards
+}
+
+function countDown(n) {
+  let i = 0
+  while (i <= n) {
+    console.log(n - i)
+    i++
+  }
+}
