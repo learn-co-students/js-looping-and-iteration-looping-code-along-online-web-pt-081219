f9560a607f88c8c659e8f7fe30dab0e4f826e263
umberto palazzo <upalazzo93@gmail.com>
2020-03-01 21:11:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(people) {
+    let messages = [];
+    for (let i = 0; i < people.length; i++) {
+
+        messages.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`);
+    }
+
+    return messages;
+} 
+
+function countDown(number) {
+    let i = 0;
+    while (i <= number) {
+        console.log(number - i);
+        i++;
+    }
+}
