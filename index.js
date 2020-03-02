c7baffe1a9d55d7b26a833d8cab7a412ea090cb0
Alec Hall <alecnhall@gmail.com>
2020-03-02 09:39:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(){
+    let countdown = 10
+    while (countdown >= 0){
+        console.log(countdown--)
+    }
+}
