511ec6365f1ae0bbdd1ef3016e34c0d781d986d7
Christian Cain <christian22cain@yahoo.com>
2020-03-02 22:41:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+// => [ 'Thank you, Lisa, for the wonderful surprise gift!', 
+// 'Thank you, Kaitlin, for the wonderful surprise gift!', 
+// 'Thank you, Jan, for the wonderful surprise gift!' ]
+
+function writeCards(inputArray, type) {
+
+    let returnArray = []
+
+    for (let i = 0; i < inputArray.length; i++) {
+        let sentence = `Thank you, ${inputArray[i]}, for the wonderful ${type} gift!`
+        returnArray.push(sentence)
+    }
+
+    return returnArray
+}
+
+function countDown(amount) {
+    let number = amount;
+    let returnArray = [];
+
+    while (number >= 0){
+        console.log(number)
+        number--;
+    }
+}
