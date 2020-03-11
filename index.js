ef61c662c7182ba9c8d91c6d2224b4d70e897632
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-11 00:35:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, surprise) { 
+    let newArray = [] 
+    for (let i = 0; i < names.length; i++)  { 
+  newArray.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`) ;
+    }
+    return newArray;
+}
+
+// cant use shovel in javascript
+   
+// counter is a number they'll pass in
+function countDown(counter){
+
+while (counter > 0) {
+    console.log(counter--);
+}
+console.log(counter)
+// will log it one last time when its zero
+}
